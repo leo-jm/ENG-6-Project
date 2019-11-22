8687dd2b3ac71cd3aa783d9f9316c95b480c816d
leo-jm <32468691+leo-jm@users.noreply.github.com>
2019-11-21 16:53:53 -0800

    Add files via upload

--- card.m
+++ card.m
@@ -1,12 +1,32 @@
-classdef card
+classdef card < handle
     properties
         value;
         played = 0;
+        inPlay = 0;
         position = 0;
     end
     methods
+        % Constructor function
         function obj = card(value)
-            obj.value = value
+            obj.value = value;
+        end
+        % Call if card is drawn from deck
+        function cardPlayed(obj)
+            obj.played = 1;
+            cardInPlay(obj)
+        end
+        
+        function cardInPlay(obj)
+            obj.inPlay = 1;
+        end
+        
+        % Call when card is discarded
+        function discard(obj)
+            obj.inPlay = 0;
+        end
+        
+        function reshuffle(obj)
+            obj.played = 0;
         end
     end   
 end
--- deck.m
+++ deck.m
@@ -4,28 +4,33 @@ classdef deck
         cards = [];
     end
     methods
+        % Constructor function
         function obj = deck(decks)
             if nargin == 0
-                obj.decks = 1
+                obj.decks = 1;
                 for j = 1:52
                     if ~isempty(obj.cards)
-                        obj.cards(end+1) = card(j)
+                        obj.cards(end+1) = card(j);
                     else
-                        obj.cards = card(j)
+                        obj.cards = card(j);
                     end
                 end
             else
-            obj.decks = decks
-            for i = 1:decks
-                for j = 1:52
-                    if ~isempty(obj.cards)
-                        obj.cards(end+1) = card(j)
-                    else
-                        obj.cards = card(j)
+                obj.decks = decks;
+                for i = 1:decks
+                    for j = 1:52
+                        if ~isempty(obj.cards)
+                            obj.cards(end+1) = card(j);
+                        else
+                            obj.cards = card(j);
+                        end
                     end
                 end
             end
-            end
+        end
+        % Draw top card
+        function drawCard(obj)
+            obj.cards(1).cardPlayed;
         end
     end
 end
