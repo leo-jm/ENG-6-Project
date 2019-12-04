87ba91dd4c300233660d9106b28d49c3304fb459
leo-jm <32468691+leo-jm@users.noreply.github.com>
2019-12-04 01:08:27 -0800

    Add files via upload

--- card.m
+++ card.m
@@ -23,114 +23,61 @@ classdef card < handle
             obj.value = cardValues(remainder+1);
             obj.altValue = altCardValues(remainder+1);
             end
-            switch cardNumber
-                case 1
-                    obj.image = imread('ace_of_spades.png');
-                case 2
-                    obj.image = imread('2_of_spades.png');
-                case 3
-                    obj.image = imread('3_of_spades.png');
-                case 4
-                    obj.image = imread('4_of_spades.png');
-                case 5
-                    obj.image = imread('5_of_spades.png');
-                case 6
-                    obj.image = imread('6_of_spades.png');
-                case 7 
-                    obj.image = imread('7_of_spades.png');
-                case 8 
-                    obj.image = imread('8_of_spades.png');
-                case 9 
-                    obj.image = imread('9_of_spades.png');
-                case 10
-                    obj.image = imread('10_of_spades.png');
-                case 11 
-                    obj.image = imread('jack_of_spades2.png');
-                case 12 
-                    obj.image = imread('queen_of_spades2.png');
-                case 13 
-                    obj.image = imread('king_of_spades2.png');
-                case 14 
-                    obj.image = imread('ace_of_hearts.png');
-                case 15
-                    obj.image = imread('2_of_hearts.png');
-                case 16
-                    obj.image = imread('3_of_hearts.png');
-                case 17
-                    obj.image = imread('4_of_hearts.png');
-                case 18
-                    obj.image = imread('5_of_hearts.png');
-                case 19
-                    obj.image = imread('6_of_hearts.png');
-                case 20
-                    obj.image = imread('7_of_hearts.png');
-                case 21
-                    obj.image = imread('8_of_hearts.png');
-                case 22
-                    obj.image = imread('9_of_hearts.png');
-                case 23
-                    obj.image = imread('10_of_hearts.png');
-                case 24
-                    obj.image = imread('jack_of_hearts2.png');
-                case 25
-                    obj.image = imread('queen_of_hearts2.png');
-                case 26
-                    obj.image = imread('king_of_hearts2.png');
-                case 27
-                    obj.image = imread('ace_of_diamonds.png');
-                case 28
-                    obj.image = imread('2_of_diamonds.png');
-                case 29
-                    obj.image = imread('3_of_diamonds.png');
-                case 30
-                    obj.image = imread('4_of_diamonds.png');
-                case 31
-                    obj.image = imread('5_of_diamonds.png');
-                case 32
-                    obj.image = imread('6_of_diamonds.png');
-                case 33
-                    obj.image=imread('7_of_diamonds.png');
-                case 34
-                    obj.image=imread('8_of_diamonds.png');
-                case 35
-                    obj.image=imread('9_of_diamonds.png');
-                case 36
-                    obj.image=imread('10_of_diamonds.png');
-                case 37
-                    obj.image=imread('jack_of_diamonds2.png');
-                case 38
-                    obj.image=imread('queen_of_diamonds2.png');
-                case 39
-                    obj.image=imread('king_of_diamonds2.png');
-                case 40
-                    obj.image=imread('ace_of_clubs.png');
-                case 41
-                    obj.image=imread('2_of_clubs.png');
-                case 42
-                    obj.image=imread('3_of_clubs.png');
-                case 43
-                    obj.image=imread('4_of_clubs.png');
-                case 44
-                    obj.image=imread('5_of_clubs.png');
-                case 45
-                    obj.image=imread('6_of_clubs.png');
-                case 46
-                    obj.image=imread('7_of_clubs.png');
-                case 47
-                    obj.image=imread('8_of_clubs.png');
-                case 48
-                    obj.image=imread('9_of_clubs.png');
-                case 49
-                    obj.image=imread('10_of_clubs.png');
-                case 50
-                    obj.image=imread('jack_of_clubs2.png');
-                case 51
-                    obj.image=imread('queen_of_clubs2.png');
-                case 52  
-                    obj.image=imread('king_of_clubs2.png');
-                case 0 
-                    obj.image=imread('joker.png');
-             end
+            cardImages = ["joker.png";
+                          'ace_of_spades.png';
+                          '2_of_spades.png';
+                          '3_of_spades.png';
+                          '4_of_spades.png';
+                          '5_of_spades.png';
+                          '6_of_spades.png';
+                          '7_of_spades.png';
+                          '8_of_spades.png';
+                          '9_of_spades.png';
+                          '10_of_spades.png';
+                          'jack_of_spades2.png';
+                          'queen_of_spades2.png';
+                          'king_of_spades2.png';
+                          'ace_of_hearts.png';
+                          '2_of_hearts.png';
+                          '3_of_hearts.png';
+                          '4_of_hearts.png';
+                          '5_of_hearts.png';
+                          '6_of_hearts.png';
+                          '7_of_hearts.png';
+                          '8_of_hearts.png';
+                          '9_of_hearts.png';
+                          '10_of_hearts.png';
+                          'jack_of_hearts2.png';
+                          'queen_of_hearts2.png';
+                          'king_of_hearts2.png';
+                          'ace_of_diamonds.png';
+                          '2_of_diamonds.png';
+                          '3_of_diamonds.png';
+                          '4_of_diamonds.png';
+                          '5_of_diamonds.png';
+                          '6_of_diamonds.png';
+                          '7_of_diamonds.png';
+                          '8_of_diamonds.png';
+                          '9_of_diamonds.png';
+                          '10_of_diamonds.png';
+                          'jack_of_diamonds2.png';
+                          'queen_of_diamonds2.png';
+                          'king_of_diamonds2.png';
+                          'ace_of_clubs.png';
+                          '2_of_clubs.png';
+                          '3_of_clubs.png';
+                          '4_of_clubs.png';
+                          '5_of_clubs.png';
+                          '6_of_clubs.png';
+                          '7_of_clubs.png';
+                          '8_of_clubs.png';
+                          '9_of_clubs.png';
+                          '10_of_clubs.png';
+                          'jack_of_clubs2.png';
+                          'queen_of_clubs2.png';
+                          'king_of_clubs2.png';];
+            obj.image = imread(strcat(".\card_images\",cardImages(cardNumber + 1)));       
+                    
         end
         
         % Call if card is drawn from deck
